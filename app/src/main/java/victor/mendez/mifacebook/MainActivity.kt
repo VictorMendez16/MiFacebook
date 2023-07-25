c23bbf26fbff29958c2f95ddfcd0ba7a6af1589b
victor <victorcasiano1693@gmail.com>
2023-07-24 19:32:18 -0700

    Initial commit

---
+++ app/src/main/java/victor/mendez/mifacebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package victor.mendez.mifacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
